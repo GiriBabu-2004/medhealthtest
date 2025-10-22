92da1c711ba161b143ff8cbcd0c4c37f59fd8b68
GiriBabu-2004 <sujaykumargiri550@gmail.com>
2025-10-22 11:58:10 +0530

    feat(upload-prescription): add initial route and page structure for prescription upload

---
+++ frontend/app/upload-prescription/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex justify-center mt-50 font-bold text-4xl'>this the page where subhayan baal add his efforts</div>
+  )
+}
+
+export default page
