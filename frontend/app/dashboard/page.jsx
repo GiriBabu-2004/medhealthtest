1dfc99b16358cf2ad26e4e23f88eef0b0ba88288
GiriBabu-2004 <sujaykumargiri550@gmail.com>
2025-10-25 12:57:56 +0530

    feat(Dashboard): enhance feedback button functionality and integrate modal for user input

--- frontend/app/dashboard/page.jsx
+++ frontend/app/dashboard/page.jsx
@@ -13,10 +13,14 @@ import Footer from '@/components/Footer';
 import Feedback from '@/components/Feedback';
 import Chatbot from '@/components/Chatbot';
 import FAQSection from '@/components/FAQsection';
+import { Edit2 } from 'lucide-react';
+import { ArrowRight } from 'lucide-react';
+import { ChevronRight } from 'lucide-react';
 
 export default function Dashboard() {
   const [user, setUser] = useState(null);
     const [showFeedback, setShowFeedback] = useState(false);
+    const [showModal, setShowModal] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
@@ -54,26 +58,41 @@ export default function Dashboard() {
 
           
 {/* Feedback Floating Button */}
-<button
-  onClick={() => setShowFeedback(true)}
-  className="fixed right-6 top-32 bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-full shadow-lg transition-all duration-300 z-40"
+<div
+  className={`fixed top-1/3 left-0 transform transition-transform duration-700 z-40 ${
+    showFeedback ? 'translate-x-0' : '-translate-x-[80%]'
+  }`}
 >
-  💬 Feedback
-</button>
+  <button
+    onClick={() => {
+      if (!showFeedback) {
+        // First click → show full button
+        setShowFeedback(true);
+        // Auto-hide after 5s
+        setTimeout(() => setShowFeedback(false), 5000);
+      } else {
+        // Second click → open popup
+        setShowModal(true);
+      }
+    }}
+    className="bg-green-500 hover:bg-green-600 text-white px-1 py-3 rounded-r-full shadow-lg transition-all duration-300 flex items-center gap-2"
+  >
+    FeedBack <span className="hidden sm:inline"><ChevronRight className='w-5 h-5'/></span>
+  </button>
+</div>
 
 {/* Feedback Popup Modal */}
-{showFeedback && (
+{showModal && (
   <div
     className="fixed inset-0 bg-black/50 flex items-center justify-center z-50"
     onClick={(e) => {
-      // close when clicking outside form
-      if (e.target === e.currentTarget) setShowFeedback(false);
+      if (e.target === e.currentTarget) setShowModal(false);
     }}
   >
-    <div className="bg-white  max-w-md p-6  shadow-lg relative animate-fadeIn">
+    <div className="bg-white max-w-md p-6 shadow-lg relative animate-fadeIn ">
       {/* Close Button */}
       <button
-        onClick={() => setShowFeedback(false)}
+        onClick={() => setShowModal(false)}
         className="absolute top-3 right-3 text-gray-500 hover:text-red-700 text-xl font-bold cursor-pointer"
       >
         &times;
@@ -83,6 +102,7 @@ export default function Dashboard() {
     </div>
   </div>
 )}
+
 
         </>
       )}
