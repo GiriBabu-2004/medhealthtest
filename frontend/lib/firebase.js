0037ee7f5abbd58e82d0f275ac322068ddc45ab2
Subhayan Paul <subhayanp.cse.jisu22@gmail.com>
2026-04-28 23:43:21 +0530

    Obfuscate Firebase configuration values
    Replaced sensitive Firebase configuration values with placeholders.

--- frontend/lib/firebase.js
+++ frontend/lib/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDXqYzEyPp6YJlY2H8xNR2cD9tXahje0e8",
-  authDomain: "medhealth-7e194.firebaseapp.com",
-  projectId: "medhealth-7e194",
-  storageBucket: "medhealth-7e194.firebasestorage.app",
-  messagingSenderId: "868979980173",
-  appId: "1:868979980173:web:e068a44eea0434a41c2fc4",
-  measurementId: "G-C8MX76GHR2"
+  apiKey: "..................",
+  authDomain: "................",
+  projectId: "................",
+  storageBucket: "................",
+  messagingSenderId: "..................",
+  appId: ".....................",
+  measurementId: "..................."
 };
 
 const app = initializeApp(firebaseConfig);
